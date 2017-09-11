a8af7e516d36d7fbf77b3af8597cc101210446f9
Ventura Del Monte <venturadelmonte@gmail.com>
2017-09-11 15:11:07 +0200

    Extend native codes support to Java

--- src/java/jni/genconst.c
+++ src/java/jni/genconst.c
@@ -15,7 +15,7 @@
  * - The name of the author may not be used to endorse or promote products
  *   derived from this software without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS"" AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
@@ -28,6 +28,7 @@
  */
 #include <stdio.h>
 #include <papi.h>
+#include <string.h>
 
 #define JAVA_HEADER "/*\n" \
 	" * This is generated file. Do not edit.\n" \
@@ -36,7 +37,7 @@
 	" */\n\n" \
 	"package papi;\n" \
 	"\n" \
-	"class Constants {"
+	"public class Constants {"
 
 #define JAVA_FOOTER "}"
 
@@ -55,10 +56,109 @@ int main() {
 	PRINT_CONST(PAPI_OK);
 	PRINT_CONST(PAPI_EINVAL);
 
-	START_GROUP("Preset events");
-	PRINT_CONST(PAPI_TOT_CYC);
-	PRINT_CONST(PAPI_L1_DCM);
+	START_GROUP("Mixed events");
+	//PRINT_CONST(PAPI_TOT_CYC);
+	//PRINT_CONST(PAPI_L1_DCM);
 
+      	PAPI_library_init(PAPI_VER_CURRENT);
+	int event_code;
+	PAPI_event_info_t info;
+
+	char* names[] = {
+			"CPU_CLK_UNHALTED:THREAD_P",
+			"IDQ_UOPS_NOT_DELIVERED:CORE",
+			"UOPS_ISSUED",
+			"UNHALTED_CORE_CYCLES",
+			"RESOURCE_STALLS:ANY",
+			"INT_MISC:RECOVERY_CYCLES",
+			"UOPS_RETIRED:RETIRE_SLOTS",
+			"CYCLE_ACTIVITY:CYCLES_LDM_PENDING",
+			"UOPS_EXECUTED:CORE_CYCLES_NONE",
+			"UOPS_EXECUTED:CORE:c=1",
+			"UOPS_EXECUTED:CORE:c=2",
+			"CYCLE_ACTIVITY:STALLS_L1D_MISS",
+			"CYCLE_ACTIVITY:STALLS_L2_MISS",
+			"perf::L1-ICACHE-LOAD-MISSES",
+			//"ICACHE_16B:IFDATA_STALL",
+			"INST_RETIRED:ANY_P"
+			//"CYCLE_ACTIVITY:STALLS_L1D_MISS",
+			//"CYCLE_ACTIVITY:STALLS_L2_MISS",
+			"CYCLE_ACTIVITY:STALLS_L3_MISS",
+			"CYCLE_ACTIVITY:STALLS_MEM_ANY",
+			"ICACHE_64B:IFTAG_STALL",
+			"ICACHE_16B:IFDATA_STALL",
+			"IDQ_UOPS_NOT_DELIVERED:CYCLES_0_UOPS_DELIV_CORE",
+			"IDQ_UOPS_NOT_DELIVERED:CYCLES_LE_1_UOPS_DELIV_CORE",
+			"IDQ_UOPS_NOT_DELIVERED:CYCLES_LE_2_UOPS_DELIV_CORE"
+			"IDQ_UOPS_NOT_DELIVERED:CYCLES_LE_3_UOPS_DELIV_CORE",
+			"IDQ_UOPS_NOT_DELIVERED:CYCLES_FE_WAS_OK"
+			"UOPS_EXECUTED:CORE_CYCLES_NONE",
+			"UOPS_EXECUTED:CORE_CYCLES_GE_1",
+			"UOPS_EXECUTED:CORE_CYCLES_GE_2"
+			"UOPS_EXECUTED:CORE_CYCLES_GE_3",
+			"UOPS_EXECUTED:CORE_CYCLES_GE_4",
+			"PAPI_L1_DCM",
+			"PAPI_L2_DCM",
+			"PAPI_L3_TCM",
+			"PAPI_BR_MSP",
+			"PAPI_BR_TKN",
+			"PAPI_BR_NTK",
+			"PAPI_MEM_WCY",
+			"PAPI_STL_ICY",
+			"PAPI_FUL_ICY",
+			"PAPI_STL_CCY",
+			"PAPI_FUL_CCY",
+			"PAPI_RES_STL"
+			"UOPS_ISSUED",
+			"PAPI_TOT_CYC",
+			"CYCLE_ACTIVITY:STALLS_TOTAL"
+			"PAPI_L1_ICM",
+			"PAPI_L2_ICM",
+			"PAPI_TLB_IM",
+			NULL
+	};
+
+	for (int i = 0; names[i] != NULL; i++) {
+		if (PAPI_event_name_to_code(names[i], &event_code) == PAPI_OK ) {
+			if (PAPI_get_event_info(event_code, &info) == PAPI_OK ) {
+					char buffer[1024] = { 0 };
+
+					if (strstr(names[i], ":") != NULL) {
+						char tmp[256];
+						strcpy(tmp, names[i]);
+						char* token = strtok(tmp, ":");
+						sprintf(buffer, "%s", token);
+						while (1) {
+							token = strtok(NULL, ":");
+							if (token == NULL) {
+								break;
+							}
+							sprintf(buffer, "%s_%s", buffer, token);
+						}
+					} else {
+						sprintf(buffer, "%s", names[i]);
+					}
+
+					char* equal = strstr(buffer, "=");
+					if (equal != NULL) {
+						*equal = '_';
+					}
+
+					char* dash = strstr(buffer, "-");
+					while (dash != NULL) {
+						*dash = '_';
+						dash = strstr(buffer, "-");
+					}
+
+					printf("\tpublic static final int %s = %d;\n", buffer, info.event_code);
+					printf("\tpublic static final String %s_name = \"%s\";\n", buffer, info.symbol);
+				} else {
+					//fprintf(stderr, "error %s %d\n",names[i],1);
+				}
+			} else {
+			//	fprintf(stderr, "error %s %d\n",names[i],2);
+			}
+	}
 
 	printf("%s\n", JAVA_FOOTER);
 
